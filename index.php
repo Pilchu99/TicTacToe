93dceaec3c3abc1edad0b4f920378ec3c7b638bf
Adam Pilch <pilchu99@gmail.com>
2019-05-20 23:21:37 +0200

    ADD GUI

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
 <!doctype html>
 <html>
 <head>
-
+<title>Tic Tac Toe</title>
+<meta charset="utf-8"/>
+<link rel="stylesheet"href="style.css" />
 </head>
 <body>
+
+<div id="main" >
+    
+    X<input type="radio" value="X" name="choice"/> <br>
+    O<input type="radio" value="O" name="choice"/><br>
+    <table>
+        <tr>
+            <td class="horizontal_line"><img src="" class="field"></td>
+            <td class="both_line"  ><img src=""class="field"></td>
+            <td class="both_line"><img src=""class="field"></td>
+        </tr>
+        <tr >
+            <td class="horizontal_line"><img src=""class="field"></td>
+            <td class="both_line"  ><img src=""class="field"></td>
+            <td class="both_line"><img src=""class="field"></td>
+        </tr>
+        <tr>
+            <td ><img src=""class="field"></td>
+            <td class="vertical_line"></td><img src=""class="field"></td>
+            <td class="vertical_line"><img src=""class="field"></td>
+        </tr>
+    
+    
+    </table>
+ 
+
+
+</div>
 
 
 </body>
