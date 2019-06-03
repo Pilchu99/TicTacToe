f5f60c32a468ee9c71ace94c42507ea8ea730cf2
Adam Pilch <pilchu99@gmail.com>
2019-06-03 15:34:10 +0200

    Fix nickname window

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 
-  function showPrompt(){
+  function showPrompt(prompt_id,player){
     
-    $('#main').append('<div id="prompt1"> <p id="p_nickname">What is your nickname?</p><input id="get_nickname" type="text" placeholder="" /><div id="option_div"><br><input id="back"type="button" value="Back" /><br><input id="next"type="button" value="Next" /></div></div>');
+    $('#main').append('<div id="'+prompt_id+'"> <p>What is your nickname?</p><input id="'+player+'" type="text" placeholder="" /><div id="option_div"><br><input id="back"type="button" value="Back" /><br><input id="next"type="button" value="Next" /></div></div>');
     var txt="Type your nickname here.";
     $('#get_nickname').attr("placeholder", txt);
     var  i=0;
@@ -21,8 +21,15 @@ $(document).ready(function(){
            
            
 }
-showPrompt();
-
+showPrompt('prompt1','player1');
+$("#next").click(function(){
+  
+  $("#prompt1").remove();
+  showPrompt('prompt2','player2');
+  $("#next").click(function(){
+  $("#prompt2").remove();
+  });
+  });
 
 
 
